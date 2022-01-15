1fc7e80ab0af02c72f23623e2572435b97c2d3ca
Tolga İskender <tolgaiskenderr@gmail.com>
2022-01-15 14:53:09 +0300

    Add files via upload

---
+++ frameworkWithDependecies.xcframework/ios-arm64/frameworkWithDependecies.framework/Headers/frameworkWithDependecies.h
@@ -0,0 +1,18 @@
+//
+//  frameworkWithDependecies.h
+//  frameworkWithDependecies
+//
+//  Created by Tolga İskender on 15.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for frameworkWithDependecies.
+FOUNDATION_EXPORT double frameworkWithDependeciesVersionNumber;
+
+//! Project version string for frameworkWithDependecies.
+FOUNDATION_EXPORT const unsigned char frameworkWithDependeciesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <frameworkWithDependecies/PublicHeader.h>
+
+
